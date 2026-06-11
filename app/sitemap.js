a5420a6e357d97a53547b3e4a869709c0f73c2a7
Prateek Vajpayee <161966608+prat1854@users.noreply.github.com>
2026-06-12 00:46:36 +0530

    Update new file sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+    return [
+      {
+        url: 'https://my-portfolio-six-teal-67.vercel.app',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 1,
+      },
+    ];
+  }
